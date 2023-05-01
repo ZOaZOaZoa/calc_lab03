c94bdb513c0b7811ec48d0eb85624901ad077b2d
ZOaZOaZoa <vbifbk555@gmail.com>
2023-05-01 18:54:53 +0300

    added parallel computing for integrating

--- task2.cpp
+++ task2.cpp
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <vector>
 #include <iomanip>
+#include <chrono>
+#include <omp.h>
 
 double f(double x)
 {
@@ -20,26 +22,45 @@ double sum_interval(double start, double end, double step)
     return sum;
 }
 
+double sum_interval_parallel(double start, double end, double step, int thread_count)
+{
+    omp_set_num_threads(thread_count);
+    std::vector<double> thread_sum(thread_count);
+    size_t steps = std::round((end-start)/step);
+    #pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        thread_sum[ID] = 0;
+        #pragma omp for
+        for(size_t i = 0; i <= steps; i++)
+        {
+            thread_sum[ID] += f(start + i*step);
+        }
+    }
+
+    double sum = 0;
+    for (int i = 0; i < thread_count; i++)
+    {
+        sum += thread_sum[i];
+    }
+
+    return sum;
+}
+
 double simpson(double f(double), double a, double b, double h)
 {
     double sum_x = sum_interval(a + h, b - h, h);
-    /*
-    double sum_x = 0;
-    size_t n = (int)((b-a)/h + 1);
-    double h_new = (b-a)/n;
-    for(int i = 1; i < n; i++)
-    {
-        sum_x += f(a + i*h_new);
-    }
-    */
+
     double sum_halfX = sum_interval(a + h/2, b - h/2, h);
-    /*
-    double sum_halfX = 0;
-    for(int i = 1; i < 2*n; i++)
-    {
-        sum_halfX += f(a + i*h_new/2);
-    }
-    */
+
+    return h/6*(f(a) + 4*sum_halfX + 2*sum_x + f(b));
+}
+
+double simpson_parallel(double f(double), double a, double b, double h, int thread_count)
+{
+    double sum_x = sum_interval_parallel(a + h, b - h, h, thread_count);
+
+    double sum_halfX = sum_interval_parallel(a + h/2, b - h/2, h, thread_count);
 
     return h/6*(f(a) + 4*sum_halfX + 2*sum_x + f(b));
 }
@@ -78,9 +99,10 @@ int main()
     double EPS = 3e-11;
     const double J = 77/(2*log(4)) - 15/(2*pow(log(4), 2));
     const double K = 15;
-    const double I_K = 8;
+    const double I_K = 9;
     const double a = 0;
     const double b = 1;
+    const int THREAD_COUNT = 728;
 
     std::vector<double> h(K);
 
@@ -89,23 +111,48 @@ int main()
         h[i-1] = (b-a)/pow(10, i);
     }
 
+    /*//Testing speed of algorithm on different values of thread_count
+    for(int thread_count = 50; thread_count < 1001; thread_count++)
+    {
+        auto start = std::chrono::steady_clock::now();
+
+        std::vector<double> Ik(I_K);
+        for(int i = 0; i < I_K; i++)
+        {
+            //std::cout << "h=" << h[i] << std::endl;
+            Ik[i] = simpson_parallel(f, a, b, h[i], thread_count);
+        }
+
+        auto end = std::chrono::steady_clock::now();
+        std::cout << "thread_count: " << std::setw(3) << thread_count << " Elapsed time: " << std::fixed 
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << "ms." << std::endl;
+    }
+    */
+
+    auto start = std::chrono::steady_clock::now();
+
     std::vector<double> Ik(I_K);
 
     for(int i = 0; i < I_K; i++)
     {
         std::cout << "h=" << h[i] << std::endl;
-        Ik[i] = simpson(f, a, b, h[i]);
+        Ik[i] = simpson_parallel(f, a, b, h[i], THREAD_COUNT);
     }
+
+    auto end = std::chrono::steady_clock::now();
 
     std::cout << "--------------------------------------\n";
 
     std::cout << "Integration. Precise value: " << std::setprecision(15) <<  J << std::endl;
 
     print_results(I_K, h, Ik, J);
+    std::cout << "Elapsed time: " << std::fixed 
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << "ms." << std::endl;
 
     std::cout << "--------------------------------------\n";
 
-    std::cout << "Integration with EPS precision. Precise value: " << std::setprecision(15) << std::fixed <<  J << ". EPS: " << std::scientific << EPS <<  std::endl;
+    std::cout << "Integration with EPS precision. Precise value: " << std::setprecision(15) << std::fixed <<  J 
+                << ". EPS: " << std::scientific << std::setprecision(2) << EPS <<  std::endl;
 
     const size_t MAX_ITER = 20;
     const size_t P = 4;
@@ -125,7 +172,6 @@ int main()
     } while (it < MAX_ITER & fabs(rung[it++]) > EPS);
     
     print_results(it, h, I, rung);
-    
 
     return 0;
 }
